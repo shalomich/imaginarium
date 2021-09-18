f9ae6f829bbec522a80f14dd14ed28143525382e
shalomich <shalo6947@gmail.com>
2021-09-18 09:59:10 +0700

    initial commit

---
+++ Imaginarium.Bot/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Imaginarium.Bot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
